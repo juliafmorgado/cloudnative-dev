34184eb09f3b541b543e3ba3239b55f1e6c485cb
Julia Furst Morgado <juliafmorgado@gmail.com>
2024-08-08 12:34:00 -0400

    complete challenge4

--- challenge-4/dashboard/server.js
+++ challenge-4/dashboard/server.js
@@ -11,7 +11,7 @@ const io = socketIo(server);
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'db', // 'db' is the name of the service in docker-compose.yml
+  host: 'db',
   database: 'challenge4',
   password: 'password',
   port: 5432,
--- challenge-4/init.sql
+++
@@ -1,7 +0,0 @@
--- init.sql
-CREATE TABLE IF NOT EXISTS texts (
-    id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
